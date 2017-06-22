108ca7c55037bf72752a45745ca5535eb8daeb1a
MahiBala1997 <mahibalajeyakumar@gmail.com>
2017-06-22 11:43:40 +0530

    Create parse.java

---
+++ parse.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prgm;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author windows
+ */
+public class string {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String word = s.nextLine();
+         String word1 = s.nextLine();
+        int n = Integer.parseInt(word);
+         int n1 = Integer.parseInt(word);
+        int length = word.length();
+        int a = n+n1;
+        System.out.println(a);
+       
+        }
+     
+    }
+
+      
