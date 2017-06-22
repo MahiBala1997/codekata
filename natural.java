bd460a3cfd8979d1db7bbdcef53f0cc2cf051bd6
MahiBala1997 <mahibalajeyakumar@gmail.com>
2017-06-22 09:58:56 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prgm;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author windows
+ */
+public class natural {
+     public static void main(String args[]){
+        System.out.println("Enter a num to be checked");
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+       
+        int sum = 0;
+        for(int i=1;i<=a;i++){
+            
+            sum +=i;
+            
+        }
+        System.out.println("The sum is:"+sum);
+     }
+}
